ab31cf021c8be0a5af4d117157d19c3148a59b55
Lizarda <mirliz3748@gmail.com>
2021-01-01 14:04:45 +0200

    added routing

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    return <div className={classes.dialog}>
+        <NavLink to={"/messages/"+props.id}>{props.name}</NavLink>
+    </div>
+}
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className="dialog_items">
-                <div className={classes.dialog}>
-                    Liza
-                </div>
-                <div className={classes.dialog}>
-                    Kolya
-                </div>
-                <div className={classes.dialog}>
-                    Masha
-                </div>
+                <DialogItem id="1" name="Liza"/>
+                <DialogItem id="2" name="Katya"/>
+                <DialogItem id="3" name="Masha"/>
+                <DialogItem id="4" name="Dima"/>
             </div>
             <div className="messages">
                 <div className={classes.mess}>Hello</div>
